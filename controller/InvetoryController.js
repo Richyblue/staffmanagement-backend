986e4039ce29cabc8bad996813a332ca03c9cd93
Jimmy Richard <60008417+Richyblue@users.noreply.github.com>
2025-02-03 17:43:32 +0100

    Update InvetoryController.js

--- controller/InvetoryController.js
+++ controller/InvetoryController.js
@@ -78,3 +78,14 @@ exports.DeleteIn=async(req,res)=>{
         res.status(500).json({message: "Faild to delete Inventory", error: error.message});
     }
 }
+
+exports.getInventryById=async(req, res)=>{
+    try {
+        const {id}=req.params;
+        const getallbyid=await Inventry.findAll({where:{id}  
+        });
+        res.status(200).json(getallbyid);
+    } catch (error) {
+        res.status(500).json({message: "Faild to Get Inventry data by ID", error: error.message});
+    }
+}
