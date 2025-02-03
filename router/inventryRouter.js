f93f3c5f69517d25a529ead0c1c7fa94c115ef20
Jimmy Richard <60008417+Richyblue@users.noreply.github.com>
2025-02-03 17:45:33 +0100

    Update inventryRouter.js

--- router/inventryRouter.js
+++ router/inventryRouter.js
@@ -7,6 +7,7 @@ router.post('/inventory', authMiddleware, Inventory.createInvenory);
 router.get('/inventory', authMiddleware, Inventory.getallIn);
 router.put('/inventory/:id', authMiddleware, Inventory.createInvenoryUpdate);
 router.delete('/inventory/:id', authMiddleware, Inventory.DeleteIn);
+router.get('/inventory/:id', authMiddleware, Inventory.getInventryById);
 
 
 
