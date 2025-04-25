20674c11b9ddd7e1dd975bc82881f6ab130e92c5
Jimmy Richard <60008417+Richyblue@users.noreply.github.com>
2025-04-25 11:02:10 +0100

    Update Staffrouter.js

--- router/Staffrouter.js
+++ router/Staffrouter.js
@@ -12,7 +12,7 @@ router.delete('/staff/:id',authMiddleware, Staff.deleteStaff);
 router.get('/staff/:id',authMiddleware, Staff.getStaffById);
 router.get('/staff/st',authMiddleware, Staff.getStaffSuspended);
 router.get('/staff/get/:staffId', authMiddleware, Staff.getStaffDetails);
-router.put('/staff/others/:id',authMiddleware, Staff.updateStaffOthers);
+router.put('/staff/others/:id', Staff.updateStaffOthers);
 router.put('/staff/medical/:id',authMiddleware, Staff.updateStaffMedical);
 router.put('/staff/friend/:id',authMiddleware, Staff.updateStaffFriends);
 router.put('/staff/parent/:id',authMiddleware, Staff.updateStaffParent);
