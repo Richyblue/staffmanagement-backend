133e37704b6553312991753fe6cfdc55df791312
Jimmy Richard <60008417+Richyblue@users.noreply.github.com>
2025-08-12 10:03:38 +0100

    Update LeaveRouter.js

--- router/LeaveRouter.js
+++ router/LeaveRouter.js
@@ -8,5 +8,6 @@ router.put('/Leave/:id', authMiddleware, StaffLeave.updateLeave);
 router.get('/Leave/:staffId', authMiddleware, StaffLeave.getLeaveStaffId);
 router.get('/Leave', authMiddleware, StaffLeave.getLeave);
 router.delete('/Leave/:id', authMiddleware, StaffLeave.deleteLeave);
+router.get('/Leavecount', authMiddleware, StaffLeave.getLeavecount);
 
 module.exports=router;
